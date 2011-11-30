c97cf8ad23ec14f1553a6ce554f3d3fec2a405b9
John Ryding <linus@johnryding.com>
2011-11-30 08:10:17 -0500

    Add the fast solution to problem 1

--- Euler/Problem 1/problem_1.rb
+++ Euler/Problem 1/problem_1.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+# Slow Solution - O(n)
+# --------------------
+# This solution is great for low numbers, but is slooooooow
+# for a number like 1,000,000,000.
+#
 sum = 0 
 
 (0..999).each do |n|
@@ -9,3 +14,33 @@ sum = 0
 end
 
 p sum
+
+# Fast Solution [1] - O(1)
+# --------------------
+# We are really just doing a summation here, so we should actually take advantage
+# of those well known summation formulas we learned in school!
+#
+# For example, the summation of all numbers < 1000 that are divisible by 3 is:
+#
+#   3+6+9+...+999 = 3 * (1+2+3+...+333)
+#
+# We did the equation on the left in the "Slow Solution", but the equation on the
+# right is a normal 1 to n summation[2]:
+#
+# 3*Sum(1..n) = 3 * ([n^2 + n]/2) = 3 * [(1/2)*n*(n+1)]
+#
+# So now our loop, which executes N times, executes once and just computes a formula
+# (something computers are really good at).
+#
+# -----------------
+# [1] http://projecteuler.net/project/resources/001_c619a145e9d327a5c4c84649bec9981b/001_overview.pdf
+# [2] http://en.wikipedia.org/wiki/Summation
+TARGET = 999999999
+
+def sum_divisible_by(div)
+  n = TARGET / div
+  return div * ( (0.5)*n*(n+1) )
+end
+
+puts sum_divisible_by(3) + sum_divisible_by(5) - sum_divisible_by(15)
+
